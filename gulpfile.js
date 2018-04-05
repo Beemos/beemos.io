f7496e4f65f9399a76cbb06d8c44a715fe6b5900
jesse <jesseaborden@gmail.com>
2018-04-04 19:16:33 -0700

    Add gulp task to minify css

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var sass = require('gulp-sass');
 gulp.task('sass', function () {
   return gulp.src('css/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(sass({outputStyle: 'compressed'}))
     .pipe(gulp.dest('./css/built'));
 });
 
